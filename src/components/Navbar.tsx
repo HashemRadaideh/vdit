a7ad5a549066c9562d8da8154f7f9e9ec0d5d2f7
HashemRadaideh <hashemalradaideh@gmail.com>
2024-04-06 04:39:33 +0300

    Styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,35 @@
 import { ThemeToggle } from "./ThemeToggle";
 
 export const Navbar = () => {
-  return (
-    <nav className="flex justify-between bg-primary text-xl text-secondary">
-      <a className="logo p-4" href="/">
-        Vdit
-      </a>
+	return (
+		<nav className="flex justify-between bg-primary-600 text-xl text-secondary">
+			<a className="bg-primary-700 p-4" href="/">
+				Vdit
+			</a>
 
-      <div className="flex justify-between">
-        <ul className="flex py-4">
-          <li>
-            <ThemeToggle />
-          </li>
+			<div className="flex justify-between">
+				<ul className="flex gap-8 bg-primary-700 p-4">
+					<li>
+						<ThemeToggle />
+					</li>
 
-          <li>
-            <a className="p-4" href="/home">
-              Home
-            </a>
-          </li>
+					<li>
+						<a href="/home">Home</a>
+					</li>
 
-          <li>
-            <a className="p-4" href="/about">
-              About
-            </a>
-          </li>
+					<li>
+						<a href="/about">About</a>
+					</li>
 
-          <li>
-            <a className="p-4" href="/chat">
-              chat
-            </a>
-          </li>
-        </ul>
-        <div className="p-4">profile</div>
-      </div>
-    </nav>
-  );
+					<li>
+						<a href="/chat">chat</a>
+					</li>
+
+					<li>
+						<a href="/profile">profile</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
